1f1b9d1a7fbe20e4c13fece61b286f0c8900152b
stefankablowski <stefan.kablowski@gmail.com>
2022-08-22 14:10:57 +0200

    brute force generate corrections

--- src/generateCorrections.ts
+++ src/generateCorrections.ts
@@ -3,21 +3,83 @@ import {Alphabet} from './model/Alphabet';
 import {Deletion, Insertion, Replacement} from './model/EditOperation';
 import {Correction} from './model/EditOperation/Correction';
 import {EditOperation} from './model/EditOperation/EditOperation';
+import {Grammar} from './model/Grammar';
+import {parseAndEncode, translateGrammar} from './translateGrammar';
 
-export function generateCorrectionsOfLength(
+export function generateAllMinimalCorrections(word: Word, grammar: Grammar) {
+  // compute all corrections of length 1
+  const operations: EditOperation[] = generateOperationsForWord(
+    word,
+    grammar.terminals
+  );
+  // apply these corrections to the word
+  const words: Word[] = applyOperationsToWord(operations, word);
+  // remove all corrections where w e L and save them in minimal correction set
+  const [minimal, remaining] = checkWordProblemForWords(words, grammar);
+
+  // extend remaining corrections by all possible edit operations each
+  let previousRemainingFixPoint = 0;
+  let currentRemainingFixPoint = remaining.length;
+  while (previousRemainingFixPoint < currentRemainingFixPoint) {
+    previousRemainingFixPoint = currentRemainingFixPoint;
+    currentRemainingFixPoint = remaining.length;
+  }
+}
+
+export function checkWordProblemForWords(
+  words: Word[],
+  grammar: Grammar
+): [minimal: Word[], remaining: Word[]] {
+  const minimal: Word[] = [];
+  const remaining: Word[] = [];
+  const lexicon = new Map<string, string>();
+  const exprGrammar = translateGrammar(grammar, lexicon);
+  words.forEach(word => {
+    const parseTree = parseAndEncode(lexicon, exprGrammar, word);
+    if (parseTreeContainsParses(parseTree)) {
+      minimal.push(word);
+    } else {
+      remaining.push(word);
+    }
+  });
+  return [minimal, remaining];
+}
+
+export function generateOperationsForWord(
   word: Word,
-  alphabet: Alphabet,
-  length: number
-): Correction {
+  alphabet: Alphabet
+): EditOperation[] {
   const operations: EditOperation[] = [];
 
   for (let index = 0; index < word.length; index++) {
     const currentToken = word[index];
     operations.push(new Deletion(currentToken, index));
     for (const symb of alphabet) {
-      operations.push(new Insertion(symb, index));
-      operations.push(new Replacement(currentToken, symb, index));
+      // operations.push(new Insertion(symb, index));
+      if (symb !== currentToken) {
+        operations.push(new Replacement(currentToken, symb, index));
+      }
     }
   }
-  return new Correction(operations);
+  return operations;
+}
+
+export function applyOperationsToWord(
+  operations: EditOperation[],
+  word: Word
+): Word[] {
+  return operations.map(op => op.apply(word));
+}
+
+export function sortOperationsByType(
+  operations: EditOperation[]
+): EditOperation[] {
+  const insertions = operations.filter(op => op instanceof Insertion);
+  const replacements = operations.filter(op => op instanceof Replacement);
+  const deletions = operations.filter(op => op instanceof Deletion);
+  return [...insertions, ...replacements, ...deletions];
+}
+
+function parseTreeContainsParses(parseTree: any[]) {
+  return parseTree.length > 0;
 }
---
+++ tests/generateCorrections.test.ts
@@ -0,0 +1,278 @@
+import {translateGrammar, parseAndEncode} from '../src/translateGrammar';
+import {
+  generateOperationsForWord,
+  applyOperationsToWord,
+  sortOperationsByType,
+  checkWordProblemForWords,
+} from '../src/generateCorrections';
+import {Correction} from '../src/model/EditOperation/Correction';
+import {EditOperation} from '../src/model/EditOperation/EditOperation';
+import {Word} from '../src/model/Word';
+import {Deletion, Insertion, Replacement} from '../src/model/EditOperation';
+
+describe('generateCorrections', () => {
+  test('Correction Biogrammar', () => {
+    const grammar = {
+      rules: new Map([
+        ['hypothese', [['einfach'], ['komplex']]],
+        ['einfach', [['unspezifisch'], ['spezifisch'], ['konditional']]],
+        ['komplex', [['einfach', 'modal'], ['vergleich']]],
+
+        [
+          'unspezifisch',
+          [
+            [
+              'der|die|das|den',
+              'unspezvariable',
+              'unspezabhaengigkeit',
+              'der|die|das|den',
+              'unspezvariable',
+            ],
+          ],
+        ],
+        [
+          'spezifisch',
+          [
+            [
+              'der|die|das|den',
+              'unspezvariable',
+              'spezabhaengigkeit',
+              'spezAdjektiv',
+              'bei',
+              'unspezvariable',
+            ],
+            [
+              'bei',
+              'Adjektiv',
+              'unspezvariable',
+              'spezabhaengigkeit',
+              'der|die|das|den',
+              'unspezvariable',
+            ],
+            [
+              'bei',
+              'spezUV',
+              'spezabhaengigkeit',
+              'der|die|das|den',
+              'unspezvariable',
+              'spezAdjektiv',
+            ],
+            [
+              'der|die|das|den',
+              'unspezvariable',
+              'spezabhaengigkeit',
+              'spezAdjektiv',
+              'bei',
+              'spezUV',
+            ],
+          ],
+        ],
+        [
+          'konditional',
+          [
+            [
+              'wenn',
+              'der|die|das|den',
+              'unspezvariable',
+              'AdjektivStar',
+              'spezabhaengigkeit',
+              ', dann',
+              'spezabhaengigkeit',
+              'der|die|das|den',
+              'unspezvariable',
+              'spezAdjektiv',
+            ],
+            [
+              'je',
+              'Adjektiv',
+              'der|die|das|den',
+              'unspezvariable',
+              'spezabhaengigkeit',
+              ', desto',
+              'Adjektiv',
+              'spezabhaengigkeit',
+              'der|die|das|den',
+              'unspezvariable',
+            ],
+          ],
+        ],
+        ['modal', [[', aber nur', 'Wertebereich']]],
+        [
+          'vergleich',
+          [
+            [
+              'Adjektiv',
+              'unspezvariable',
+              'unspezabhaengigkeit',
+              'der|die|das|den',
+              'unspezvariable',
+              'vergleichsOp',
+              'Adjektiv',
+              'unspezvariable',
+            ],
+            [
+              'der|die|das|den',
+              'unspezAV',
+              'spezabhaengigkeit',
+              'bei',
+              'spezUV',
+              'vergleichsOp',
+              'bei',
+              'spezUV',
+            ],
+          ],
+        ],
+
+        ['unspezvariable', [['unspezAV'], ['unspezUV']]],
+
+        [
+          'unspezAV',
+          [
+            ['Hefeaktivitaet'],
+            ['Pizzateig'],
+            ['Hefe'],
+            ['Enzyme'],
+            ['Enzymaktivitaet'],
+          ],
+        ],
+        ['unspezUV', [['Temperatur'], ['Waerme'], ['Kaelte']]],
+        ['spezUV', [['20 Grad'], ['0 Grad']]],
+        ['unspezabhaengigkeit', [['beeinflusst']]],
+        ['spezabhaengigkeit', [['steigt'], ['fällt'], ['arbeitet']]],
+        ['Adjektiv', [['mehr'], ['weniger'], ['hoeher|e'], ['niedriger|e']]],
+        ['spezAdjektiv ', [['langsamer'], ['schneller']]],
+
+        ['AdjektivStar', [['spezAdjektiv'], ['Adjektiv']]],
+        ['vergleichsOp', [['besser als']]],
+        [
+          'Wertebereich',
+          [['von 20 Grad'], ['ab 20 Grad'], ['von 20 Grad bis 40 Grad']],
+        ],
+      ]),
+      terminals: [
+        'von 20 Grad',
+        'ab 20 Grad',
+        'von 20 Grad bis 40 Grad',
+        'besser als',
+        'langsamer',
+        'schneller',
+        'niedriger|e',
+        'hoeher|e',
+        'mehr',
+        'weniger',
+        'hoeher|e',
+        'niedriger|e',
+        'steigt',
+        'fällt',
+        'arbeitet',
+        'beeinflusst',
+        '0 Grad',
+        '20 Grad',
+        '0 Grad',
+        'Hefeaktivitaet',
+        'Pizzateig',
+        'Hefe',
+        'Enzyme',
+        'Enzymaktivitaet',
+        'bei',
+        'der|die|das|den',
+        'je',
+        ', dann',
+        ', desto',
+        'Temperatur',
+        'Waerme',
+        'Kaelte',
+        ', aber nur',
+      ],
+      rootRule: 'S',
+    };
+    const lexicon = new Map<string, string>();
+    const exprGrammar = translateGrammar(grammar, lexicon);
+
+    // Die Wärme beeinflusst die Hefe
+    const sentence1 = [
+      'der|die|das|den',
+      'Waerme',
+      'beeinflusst',
+      'der|die|das|den',
+      'Hefe',
+    ];
+
+    // Höhere Wärme beeinflust die Hefeaktivität besser als weniger Wärme
+    const sentence2 = [
+      'hoeher|e',
+      'Waerme',
+      'beeinflusst',
+      'der|die|das|den',
+      'Hefeaktivitaet',
+      'besser als',
+      'weniger',
+      'Waerme',
+    ];
+    expect(
+      parseAndEncode(lexicon, exprGrammar, sentence1).length > 0
+    ).toBeTruthy();
+    expect(
+      parseAndEncode(lexicon, exprGrammar, sentence2).length > 0
+    ).toBeTruthy();
+  });
+});
+test('Apple-Banana-Grammar', () => {
+  const grammar = {
+    rules: new Map([
+      ['S', [['E']]],
+      ['E', [['E', '+', 'E'], ['Apple'], ['Banana']]],
+    ]),
+    terminals: ['Banana', 'Apple', '+'],
+    rootRule: 'S',
+  };
+  const lexicon = new Map<string, string>();
+  const exprGrammar = translateGrammar(grammar, lexicon);
+
+  const word = ['Banana', '+', 'Apple'];
+  const operations: EditOperation[] = generateOperationsForWord(
+    word,
+    grammar.terminals
+  );
+  expect(sortOperationsByType(operations)).toMatchObject([
+    // {deleteSymbol: '', insertSymbol: 'Banana', index: 0},
+    // {deleteSymbol: '', insertSymbol: 'Apple', index: 0},
+    // {deleteSymbol: '', insertSymbol: '+', index: 0},
+    // {deleteSymbol: '', insertSymbol: 'Banana', index: 1},
+    // {deleteSymbol: '', insertSymbol: 'Apple', index: 1},
+    // {deleteSymbol: '', insertSymbol: '+', index: 1},
+    // {deleteSymbol: '', insertSymbol: 'Banana', index: 2},
+    // {deleteSymbol: '', insertSymbol: 'Apple', index: 2},
+    // {deleteSymbol: '', insertSymbol: '+', index: 2},
+    {
+      deleteSymbol: 'Banana',
+      insertSymbol: 'Apple',
+      index: 0,
+    },
+    {deleteSymbol: 'Banana', insertSymbol: '+', index: 0},
+    {deleteSymbol: '+', insertSymbol: 'Banana', index: 1},
+    {deleteSymbol: '+', insertSymbol: 'Apple', index: 1},
+    {
+      deleteSymbol: 'Apple',
+      insertSymbol: 'Banana',
+      index: 2,
+    },
+    {deleteSymbol: 'Apple', insertSymbol: '+', index: 2},
+    {deleteSymbol: 'Banana', insertSymbol: '', index: 0},
+    {deleteSymbol: '+', insertSymbol: '', index: 1},
+    {deleteSymbol: 'Apple', insertSymbol: '', index: 2},
+  ]);
+  console.log(sortOperationsByType(operations));
+  // operations.forEach(op =>
+  //   console.log(
+  //     op instanceof Insertion ||
+  //       op instanceof Deletion ||
+  //       op instanceof Replacement
+  //   )
+  // );
+  const words: Word[] = applyOperationsToWord(operations, word);
+  const [minimal, remaining] = checkWordProblemForWords(words, grammar);
+  console.log(minimal);
+  console.log('---');
+  console.log(remaining);
+});
