e76c49aa8428af8c1065ef8539bea8409cc22af4
stefankablowski <stefan.kablowski@gmail.com>
2022-11-24 18:40:14 +0100

    fix grammar tex table generation

--- src/model/Grammar.ts
+++ src/model/Grammar.ts
@@ -5,20 +5,26 @@ export class Grammar {
   terminals: string[] = [];
   rootRule = '';
   static toTex(grammar: Grammar) {
-    const result = Array.from(grammar.rules)
-      .map(([LHS, RHS]) => {
-        return `\\textit{${LHS}}\\; &\\rightarrow \\; ${printRule(
-          RHS
-        )} \\\\ \n`;
-      })
-      .join('');
+    const result = `\\SetTblrInner{hspan=minimal, rowsep=1.5pt,colsep=1.5pt}
+    \\begin{tblr}{rcX[l]}`
+      .concat(
+        Array.from(grammar.rules)
+          .map(([LHS, RHS]) => {
+            return `\\textit{${LHS}} & $\\rightarrow$ & \\textit{${printRule(
+              RHS
+            )}} \\\\ \n`;
+          })
+          .join('')
+      )
+      .concat(`\\end{tblr}`);
+
     return result;
   }
 }
 
 function printRule(RHSarray: string[][]) {
   const innerRulesFormatted = RHSarray.map(RHSsingle =>
-    RHSsingle.map(variable => `\\textit{${variable}}`).join('~')
+    RHSsingle.map(variable => `(${variable})`).join('\\ ')
   );
-  return innerRulesFormatted.join('\\ \\vert \\ ');
+  return innerRulesFormatted.join('\\ $\\vert$\\ ');
 }
